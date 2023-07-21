ad717934c3dd76b73e69274a7cd65ad762f0a87e
nata2806 <vysochina_natalja@ukr.net>
2023-07-21 12:31:54 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,11 +1,10 @@
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const galleryWrapper = document.querySelector('.gallery');
 
